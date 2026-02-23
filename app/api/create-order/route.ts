1ac0702f2b19613885763075d9b3e7ff989e531c
dexterisntfunny-developer <dexteromasta@yahoo.com>
2026-02-23 11:38:47 -0500

    Update route.ts

--- app/api/create-order/route.ts
+++ app/api/create-order/route.ts
@@ -2,7 +2,7 @@
 
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
-import { supabaseServerClient } from "lib/supabaseServer";
+import { supabaseServerClient } from "lib/supabaseServer.ts";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
