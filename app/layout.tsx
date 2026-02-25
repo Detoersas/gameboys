063a4f681b5c021ea0f5a7b2f191f90a5505379b
dexterisntfunny-developer <dexteromasta@yahoo.com>
2026-02-24 20:06:16 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,19 +16,19 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
+        url: '/zna2c-tscrf-001.ico',
         media: '(prefers-color-scheme: light)',
       },
       {
-        url: '/icon-dark-32x32.png',
+        url: '/zna2c-tscrf-001.ico',
         media: '(prefers-color-scheme: dark)',
       },
       {
-        url: '/icon.svg',
+        url: '/zna2c-tscrf-001.ico',
         type: 'image/svg+xml',
       },
     ],
-    apple: '/apple-icon.png',
+    apple: 'zna2c-tscrf-001.ico',
   },
 }
 
